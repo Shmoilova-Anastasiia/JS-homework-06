88a3db94c6ef7607c58024ea2e017ef32fcc2f2c
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-02-26 21:35:06 +0300

    task-08

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,25 @@
+const formLoginEl = document.querySelector('.login-form');
+const formInputEmailEl = document.querySelector('input[type="email"]');
+const formInputPasswordlEl = document.querySelector('input[type="password"]');
+const formBtnEl = document.querySelector ('button');
+
+formLoginEl.addEventListener('submit', (event) =>{
+event.preventDefault();
+const {
+    elements: { email, password }
+  } = event.currentTarget;
+
+
+if (formInputEmailEl.value === '' && formInputPasswordlEl.value === ''){
+   return alert ('Всі поля повинні бути заповнені');
+}
+console.log(`email: ${email.value}, Password: ${password.value}`);
+  event.currentTarget.reset();
+} 
+);
+
+
+
+
+
 
