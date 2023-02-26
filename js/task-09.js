09b5c77a726b176887e08eb1b532fb9a9fc4bdce
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-02-26 21:56:19 +0300

    task-09

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,30 @@
+const bodyEl = document.querySelector('body');
+const colorBodyEl = document.querySelector('.color');
+const buttonEl = document.querySelector('.change-color');
+
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
-}
+};
+buttonEl.addEventListener('click', () => {
+bodyEl.style.backgroundColor = getRandomHexColor();
+colorBodyEl.textContent = bodyEl.style.backgroundColor;
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
