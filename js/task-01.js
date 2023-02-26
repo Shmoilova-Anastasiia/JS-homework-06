8651b209fa70139198c5aae532765373dc056bb2
Shmoilova-Anastasiia <114925791+Shmoilova-Anastasiia@users.noreply.github.com>
2023-02-26 22:26:15 +0300

    task_01

--- js/task-01.js
+++ js/task-01.js
@@ -20,30 +20,11 @@
 
 const categoriesEl = document.querySelectorAll('.item');
 console.log(`Number of categories: ${categoriesEl.length}`);
-console.log(categoriesEl);
-
-const titleEl = document.querySelectorAll('h2');
-titleEl.forEach( function (element){
-console.log(`Category: ${element.textContent}`);
-
-});
-// const list = categoriesEl[i].children;
 
 
-// console.log(list);
 
-// // // const titleEl = document.querySelectorAll('h2');
-// // // titleEl.forEach( function ([...titleEl]) {
-// // //     titleEl.classList.add('title__category'); 
-// // //     return  
-// // // });
+categoriesEl.forEach(function (element) {
+    console.log(`Category: ${element.children[0].textContent}`);
+    console.log(`Elements: ${element.children[1].children.length}`);
+})
 
-
-// // categoriesEl.forEach(function makeMessage (element, index, array) {
-// // console.log(array);
-
-// //     // const count = ul.children;
-// //     // console.log(`Category: ${h2.textContent}`,`Elements: ${count.length}`);
-// //     return;
-// // }
-// // );
